2bb692f7333fb8746b294c795c79a9f1dcf2d2a4
Gustaf Räntilä <g.rantila@gmail.com>
2022-02-14 22:38:53 +0100

    fix(ajv6): fixed path handling when dot-path begins with '[' instead of '.'

--- lib/data-path.ts
+++ lib/data-path.ts
@@ -18,15 +18,19 @@ export interface DataPath
 export function parseDataPath( error: ValidationError ): DataPath
 {
 	// Ajv 6 and 7 differ. In 6 the dataPath separator is `.`, in 7 it's `/`.
+	// The dot-form may also use brackets: ['foo'].bar for /foo/bar
 	// Since Ajv 8, it's called "instancePath"
 
 	if ( !( error.dataPath ?? ( error as any ).instancePath ) )
 		return { path: [ ], simplePath: [ ] };
 
+	const isDotPath = ( path: string ) =>
+		path.charAt( 0 ) === '.' || path.charAt( 0 ) === '[';
+
 	const value =
 		!!( error as any ).instancePath
 		? parsePath( { pointerPath: ( error as any ).instancePath } )
-		: error.dataPath?.charAt( 0 ) === '.'
+		: isDotPath( error.dataPath )
 		? parsePath( { dotPath: error.dataPath } )
 		: parsePath( { pointerPath: error.dataPath } );
 
