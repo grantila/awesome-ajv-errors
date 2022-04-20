7301e0afb5cc1a7f5194a24f3421b6f19fa3755f
Gustaf Räntilä <g.rantila@gmail.com>
2022-04-20 18:57:16 +0200

    fix(input): fixed handling if undefined input value

--- lib/prettification.test.ts
+++ lib/prettification.test.ts
@@ -1,12 +1,32 @@
-import Ajv from "ajv"
+import Ajv from 'ajv'
 
-import { setupTests } from "./test/test-prettification.js"
-import { getTestData } from "./test/iterate-test-data.js"
-
+import { prettify } from './index-node.js'
+import { setupTests } from './test/test-prettification.js'
+import { getTestData } from './test/iterate-test-data.js'
 
 const { ajv, files } = await getTestData( Ajv );
 
-describe( "prettifications", ( ) =>
+describe( 'prettifications', ( ) =>
 {
 	setupTests( ajv, files );
+} );
+
+describe( 'special cases', ( ) =>
+{
+	test( 'undefined value', ( ) =>
+	{
+		const schema = {
+			type: 'object',
+			properties: { foo: { type: 'string', const: 'bar' } }
+		};
+
+		const validate = ajv.compile( schema );
+		const data = undefined;
+
+		expect( validate( data ) ).toBe( false );
+		const msg = prettify( validate, { data: data } );
+		expect( msg.replace( /\n/g, '' ) ).toMatch(
+			/.*The type .* should be object.*undefined.*Replace.*/
+		);
+	} );
 } );
